4178798cbe807da4327bed8668845d6a72845025
Alex Lipov <tech.alexl@gmail.com>
2017-12-26 18:47:55 +0200

    Initial commit

---
+++ app/src/main/java/info/osom/ewsandroidsample/MainActivity.java
@@ -0,0 +1,44 @@
+package info.osom.ewsandroidsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.setProperty("android.javax.xml.stream.XMLInputFactory", "com.sun.xml.stream.ZephyrParserFactory");
+        System.setProperty("android.javax.xml.stream.XMLOutputFactory", "com.sun.xml.stream.ZephyrWriterFactory");
+        System.setProperty("android.javax.xml.stream.XMLEventFactory", "com.sun.xml.stream.events.ZephyrEventFactory");
+    }
+
+    public void loadClass(View view) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String className = System.getProperty("android.javax.xml.stream.XMLOutputFactory");
+        try {
+            Class spiClass;
+            if (classLoader == null) {
+                spiClass = Class.forName(className);
+            } else {
+                spiClass = classLoader.loadClass(className);
+            }
+            Object o = spiClass.newInstance();
+            Log.d("TEST", "Success");
+            Toast.makeText(this, "Success", Toast.LENGTH_LONG).show();
+        } catch (ClassNotFoundException x) {
+            Log.e("TEST", "ClassNotFoundException", x);
+            Toast.makeText(this, "ClassNotFoundException", Toast.LENGTH_LONG).show();
+            throw new RuntimeException(x);
+        } catch (Exception x) {
+            Log.e("TEST", "Exception", x);
+            Toast.makeText(this, "Exception", Toast.LENGTH_LONG).show();
+            throw new RuntimeException(x);
+        }
+    }
+}
